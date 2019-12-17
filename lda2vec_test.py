3d6de3e8f153ae8bc80b4602489608ba022d6c13
VietPhan <hoangvietit15@gmail.com>
2019-12-17 11:05:03 +0700

    them hinh minh hoa

---
+++ lda2vec_test.py
@@ -0,0 +1,3 @@
+import lda2vec
+
+model = lda2vec(num_unique_documents = 100, vocab_size = 2000 , num_topics = 100)
--- pre_processing.py
+++ pre_processing.py
@@ -131,7 +131,7 @@ source_file = "/home/vietphan/Downloads/fbcrawl/source.txt"
 with open(filename, 'w') as f:
     with open(rating_file, 'w') as f2:
         with open(source_file, 'w') as f3:
-            while(count<=200):
+            while(count<=2):
                 print("Processing "+str(count))
                 if is_non_zero_file(data_folder+str(count)+".csv"):
                     data = pd.read_csv(data_folder+str(count)+".csv",usecols=['text'])
@@ -152,8 +152,8 @@ with open(filename, 'w') as f:
             # print(data)
                     source = str(source)
                     print(source)
-                    f.write("*doc"+str(count)+":"+str(source)+"\n")
-                    f2.write("*doc"+str(count)+":"+str(source)+"\n")
+                    # f.write("*doc"+str(count)+":"+str(source)+"\n")
+                    # f2.write("*doc"+str(count)+":"+str(source)+"\n")
                     
                     for i, line in enumerate(data):
                         line = nomolize(line)
